16ac258cbbab346dbf3c7f347984db91927fae81
Nabil Hardiansa <nhardiansa@gmail.com>
2020-11-13 19:03:28 +0800

    menambahkan ke github

---
+++ nyoba/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini halaman PHP</title>
+</head>
+
+<body>
+    <h1>Hai Maniez</h1>
+</body>
+
+</html>
